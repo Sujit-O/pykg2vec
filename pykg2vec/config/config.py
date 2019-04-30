6e85ebeb878f8dd3574144fe1754694868c37f33
shihyuan.yu <shihyuay@uci.edu>
2019-04-29 18:42:46 -0700

    add kg2e_el

--- pykg2vec/config/config.py
+++ pykg2vec/config/config.py
@@ -406,7 +406,11 @@ class KG2EConfig(BasicConfig):
                  margin=1.0,
                  data='Freebase',
                  optimizer='adam',
-                 sampling="uniform"):
+                 sampling="uniform", 
+                 distance_measure="kl_divergence",
+                 cmax=0.05,
+                 cmin=5.00
+                 ):
         BasicConfig.__init__(self)
 
         self.learning_rate = learning_rate
@@ -418,6 +422,9 @@ class KG2EConfig(BasicConfig):
         self.data = data
         self.optimizer = optimizer
         self.sampling = sampling
+        self.distance_measure = distance_measure
+        self.cmax = cmax
+        self.cmin = cmin
 
 
 class ComplexConfig(BasicConfig):
