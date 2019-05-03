c65211caddc1adb3c1d6b525e872213d174e70fe
louis <louisccc@AICPS.SERVER>
2019-05-03 10:22:26 -0700

    update parameters

--- pykg2vec/example/train_RotatE.py
+++ pykg2vec/example/train_RotatE.py
@@ -11,15 +11,15 @@ from utils.trainer import Trainer
 
 def main(_):
     parser = ArgumentParser(description='Knowledge Graph Embedding with RotatE')
-    parser.add_argument('-b', '--batch', default=128, type=int, help='batch size')
+    parser.add_argument('-b', '--batch', default=1024, type=int, help='batch size')
     parser.add_argument('-t', '--tmp', default='../intermediate', type=str, help='Temporary folder')
     parser.add_argument('-ds', '--dataset', default='Freebase15k', type=str, help='Dataset')
-    parser.add_argument('-l', '--epochs', default=10, type=int, help='Number of Epochs')
-    parser.add_argument('-tn', '--test_num', default=100, type=int, help='Number of test triples')
+    parser.add_argument('-l', '--epochs', default=500, type=int, help='Number of Epochs')
+    parser.add_argument('-tn', '--test_num', default=100000, type=int, help='Number of test triples')
     parser.add_argument('-ts', '--test_step', default=5, type=int, help='Test every _ epochs')
-    parser.add_argument('-lr', '--learn_rate', default=0.01, type=float, help='learning rate')
+    parser.add_argument('-lr', '--learn_rate', default=0.0001, type=float, help='learning rate')
     parser.add_argument('-gp', '--gpu_frac', default=0.4, type=float, help='GPU fraction to use')
-    # parser.add_argument('-k', '--embed', default=50, type=int, help='Hidden embedding size')
+    parser.add_argument('-k', '--embed', default=50, type=int, help='Hidden embedding size')
     args = parser.parse_args()
 
     data_handler = DataPrep(name_dataset=args.dataset, sampling="uniform", algo='RotatE')
@@ -27,7 +27,7 @@ def main(_):
     
     config = RotatEConfig(learning_rate=args.learn_rate,
                        batch_size=args.batch,
-                       epochs=args.epochs)
+                       epochs=args.epochs, hidden_size=args.embed, margin=24)
 
     config.test_step = args.test_step
     config.test_num  = args.test_num
