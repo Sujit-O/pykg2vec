46eb86eaabf3a13c4748c7046ed10ff4a62e3bad
Shih-Yuan Yu <louisccc@Shih-Yuande-MacBook-Pro.local>
2019-05-09 10:41:05 -0700

    change generator

--- pykg2vec/utils/generator.py
+++ pykg2vec/utils/generator.py
@@ -31,8 +31,9 @@ def get_label_mat(data, bs, te, neg_rate=1):
     mat = np.zeros(shape=(bs, te), dtype=np.int8)
     for i in range(bs):
         pos_samples = len(data[i])
+        distribution_data = list(data[i])
         for j in range(pos_samples):
-            mat[i][data[i][j]] = 1
+            mat[i][distribution_data[j]] = 1
         neg_samples = neg_rate * pos_samples
         idx = list(range(te))
         arr = list(data[i])
@@ -55,7 +56,7 @@ def raw_data_generator_trans(raw_queue, processed_queue, data, batch_size, numbe
         pos_end   = batch_size * (batch_idx+1)
         
         raw_data = np.asarray(
-            [[data[x].h, data[x].r, data[x].t] for x in random_ids[pos_start:pos_end]]
+            [[data[x].h, data[x].r, data[x].t, data[x].hr_t, data[x].tr_h] for x in random_ids[pos_start:pos_end]]
         )
         raw_queue.put(raw_data)
 
@@ -131,6 +132,21 @@ def process_function_trans(raw_queue, processed_queue, te, bs, observed_triples,
 
         processed_queue.put([ph, pr, pt, nh, nr, nt])
 
+
+def process_function(raw_queue, processed_queue, te, bs, neg_rate):
+    while True:
+        raw_data = raw_queue.get()
+        
+        h = raw_data[:, 0]
+        r = raw_data[:, 1]
+        t = raw_data[:, 2]
+
+        hr_t = get_label_mat(raw_data[:, 3], bs, te, neg_rate=neg_rate)
+        rt_h = get_label_mat(raw_data[:, 4], bs, te, neg_rate=neg_rate)
+        
+        processed_queue.put([h, r, t, hr_t, rt_h])
+
+
 def worker_process_raw_data_testing(raw_queue, processed_queue):
     ''' 
     worker process that gets data from raw queue then processes and saves to processed queue.
@@ -178,17 +194,15 @@ class Generator:
         observed_triples = {(t.h, t.r, t.t): 1 for t in data}
         number_of_batches = len(data) // config.batch_size
          
-        # TODO to be optimized. 
-        if config.algo.lower() in ["tucker","tucker_v2","conve", "complex", "distmult"]:    
-            self.gen_batch()
-        elif config.algo.lower().startswith('proje'):
-            self.gen_batch_proje()
+        self.create_feeder_process(data, config.batch_size, number_of_batches)
+        
+        if config.data == 'test' or config.data == 'valid':
+            self.create_test_processer_process(config.process_num)
         else:
-            self.create_feeder_process(data, config.batch_size, number_of_batches)
-            if config.data == 'test' or config.data == 'valid':
-                self.create_test_processer_process(config.process_num)
+            if config.algo.lower() in ["tucker","tucker_v2","conve", "complex", "distmult", "proje"]:    
+                self.create_train_processor_process(config.process_num, model_config.kg_meta.tot_entity, config.batch_size, config.neg_rate)
             else:
-                self.create_train_processor_process_trans(config.process_num, data, model_config.kg_meta.tot_entity, config.batch_size, observed_triples)
+                self.create_train_processor_process_trans(config.process_num, model_config.kg_meta.tot_entity, config.batch_size, observed_triples)
 
         del model_config, hr_t_ids_train, tr_h_ids_train, relation_property
     
@@ -218,7 +232,7 @@ class Generator:
             process_worker.daemon = True
             process_worker.start()
 
-    def create_train_processor_process_trans(self, process_num, data, te, bs, observed_triples):
+    def create_train_processor_process_trans(self, process_num, te, bs, observed_triples):
         ''' special for trans-series algorithms '''
         lh = Value('i', 0)
         lr = Value('i', 0)
@@ -230,177 +244,12 @@ class Generator:
             process_worker.daemon = True
             process_worker.start()
 
-    def raw_data_generator(self, ids):
-        gen = iter(gen_id(ids))
-        bs = self.config.batch_size
-        while True:
-            batch_idx = next(gen)
-            raw_data = np.asarray([[self.train_data[x].h,
-                                    self.train_data[x].r,
-                                    self.train_data[x].t,
-                                    self.train_data[x].hr_t,
-                                    self.train_data[x].rt_h
-                                    ] for x in
-                                   self.rand_ids_train[bs * batch_idx: bs * (batch_idx + 1)]])
-
-            self.raw_queue.put(raw_data)
-
-    def raw_data_generator_proje(self, ids):
-        gen = iter(gen_id(ids))
-        bs = self.config.batch_size
-        while True:
-            batch_idx = next(gen)
-            if self.config.data.startswith('train'):
-                raw_data = np.asarray([[self.train_triples_ids[x].h,
-                                        self.train_triples_ids[x].r,
-                                        self.train_triples_ids[x].t] for x in
-                                       self.rand_ids_train[bs * batch_idx:bs * (batch_idx + 1)]])
-            elif self.config.data.startswith('test'):
-                raw_data = np.asarray([[self.test_triples_ids[x].h,
-                                        self.test_triples_ids[x].r,
-                                        self.test_triples_ids[x].t] for x in
-                                       self.rand_ids_test[bs * batch_idx:bs * (batch_idx + 1)]])
-            elif self.config.data.startswith('valid'):
-                raw_data = np.asarray([[self.valid_triples_ids[x].h,
-                                        self.valid_triples_ids[x].r,
-                                        self.valid_triples_ids[x].t] for x in
-                                       self.rand_ids_valid[bs * batch_idx:bs * (batch_idx + 1)]])
-            else:
-                raise NotImplementedError("The data type passed is wrong!")
-
-            self.raw_queue.put(raw_data)
-            # print("raw_producer", thread.name, self.raw_queue.qsize())
-    
-    def pool_process_proje(self, bs=None, n_entity=None, neg_weight=None):
-        for i in range(self.config.process_num):
-            if self.config.data.startswith('train'):
-                p = Process(target=self.process_function_train_proje, args=(bs, n_entity, neg_weight))
-            else:
-                p = Process(target=self.process_function_test_proje, args=())
-            self.process_list.append(p)
-            p.daemon = True
-            p.start()
-
-    def pool_process(self):
-        for i in range(self.config.process_num):
-            p = Process(target=self.process_function, args=())
-            self.process_list.append(p)
-            p.daemon = True
-            p.start()
-
-    def process_function(self):
-        bs = self.config.batch_size
-        te = self.data_stats.tot_entity
-        while True:
-            raw_data = self.raw_queue.get()
-            h = raw_data[:, 0]
-            r = raw_data[:, 1]
-            t = raw_data[:, 2]
-            if self.config.data.startswith('train'):
-                hr_t = get_label_mat(raw_data[:, 3], bs, te, neg_rate=self.config.neg_rate)
-                rt_h = get_label_mat(raw_data[:, 4], bs, te, neg_rate=self.config.neg_rate)
-                self.processed_queue.put([h, r, t, hr_t, rt_h])
-            elif self.config.data.startswith('test'):
-                self.processed_queue.put([h, r, t])
-            else:
-                pass
-
-    def process_function_train_proje(self, bs, n_entity, neg_weight):
-        while True:
-            raw_data = self.raw_queue.get()
-            if raw_data is None:
-                break
-            h = raw_data[:, 0]
-            r = raw_data[:, 1]
-            t = raw_data[:, 2]
-
-            hr_hr_batch = list()
-            hr_tweight = list()
-            tr_tr_batch = list()
-            tr_hweight = list()
-
-            for idx in range(bs):
-                if np.random.uniform(-1, 1) > 0:  # t r predict h
-                    temp = np.zeros(n_entity)
-                    for idx2 in np.random.permutation(n_entity)[0:n_entity // 2]:
-                        temp[idx2] = -1.0
-                    for head in self.tr_h_ids_train[(t[idx], r[idx])]:
-                        temp[head] = 1.0
-                    tr_hweight.append(temp)
-                    # tr_hweight.append(
-                    #     [1. if x in self.tr_h_ids_train[(r[idx],t[idx])] else y for
-                    #      x, y in enumerate(np.random.choice([0., -1.], size=n_entity, p=[1 - neg_weight, neg_weight]))])
-                    tr_tr_batch.append((t[idx], r[idx]))
-                else:  # h r predict t
-                    temp = np.zeros(n_entity)
-                    for idx2 in np.random.permutation(n_entity)[0:n_entity // 2]:
-                        temp[idx2] = -1.0
-                    for tail in self.hr_t_ids_train[(h[idx], r[idx])]:
-                        temp[tail] = 1.0
-                    hr_tweight.append(temp)
-                    # hr_tweight.append(
-                    #     [1. if x in self.hr_t_ids_train[(h[idx], t[idx])] else y for
-                    #      x, y in enumerate(np.random.choice([0., -1.], size=n_entity, p=[1 - neg_weight, neg_weight]))])
-                    hr_hr_batch.append((h[idx], r[idx]))
-
-            self.processed_queue.put([np.asarray(hr_hr_batch, dtype=np.int32), np.asarray(hr_tweight, dtype=np.float32),
-                                      np.asarray(tr_tr_batch, dtype=np.int32),
-                                      np.asarray(tr_hweight, dtype=np.float32)])
-
-    def process_function_test_proje(self):
-        while True:
-            raw_data = self.raw_queue.get()
-            if raw_data is None:
-                break
-            h = raw_data[:, 0]
-            r = raw_data[:, 1]
-            t = raw_data[:, 2]
-
-            self.processed_queue.put([h, r, t])
-
-    def gen_batch(self):
-        bs = self.config.batch_size
-        te = self.data_stats.tot_entity
-        if self.config.data.startswith('train'):
-            number_of_batches = len(self.train_data) // bs
-        elif self.config.data.startswith('test'):
-            number_of_batches = len(self.test_data) // bs
-        elif self.config.data.startswith('valid'):
-            number_of_batches = len(self.valid_data) // bs
-        else:
-            raise NotImplementedError("The data type passed is wrong!")
-        print("Number_of_batches:", number_of_batches)
-
-        ids = np.random.permutation(number_of_batches)
-
-        worker = Process(target=self.raw_data_generator, args=(ids,))
-        worker.daemon = True
-        self.process_list.append(worker)
-        worker.start()
-
-        self.pool_process()
-
-    def gen_batch_proje(self, n_entity=None, neg_weight=0.5):
-        bs = self.config.batch_size
-        if not n_entity:
-            n_entity = self.data_stats.tot_entity
-        if self.config.data.startswith('train'):
-            number_of_batches = len(self.train_triples_ids) // bs
-        elif self.config.data.startswith('test'):
-            number_of_batches = len(self.test_triples_ids) // bs
-        elif self.config.data.startswith('valid'):
-            number_of_batches = len(self.valid_triples_ids) // bs
-        else:
-            raise NotImplementedError("The data type passed is wrong!")
-        print("Number_of_batches:", number_of_batches)
-
-        ids = np.random.permutation(number_of_batches)
-
-        worker = Process(target=self.raw_data_generator_proje, args=(ids,))
-        worker.daemon = True
-        self.process_list.append(worker)
-        worker.start()
-        self.pool_process_proje(bs=self.config.batch_size, n_entity=n_entity, neg_weight=neg_weight)
+    def create_train_processor_process(self, process_num, te, bs, neg_rate):
+        for i in range(process_num):
+            process_worker = Process(target=process_function, args=(self.raw_queue, self.processed_queue, te, bs, neg_rate))
+            self.process_list.append(process_worker)
+            process_worker.daemon = True
+            process_worker.start()
 
 def test_generator_proje():
     from config.config import ProjE_pointwiseConfig
@@ -451,7 +300,7 @@ def test_generator():
     config = TransEConfig()
     config.set_dataset("Freebase15k")
 
-    gen = Generator(config=GeneratorConfig(data='train', algo='transe'), model_config=config)
+    gen = Generator(config=GeneratorConfig(data='train', algo='tucker'), model_config=config)
 
     print("----init time:", timeit.default_timer() - start_time)
     for i in range(10):
