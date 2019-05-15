f0b44d2fbe58803efba1fdbb6368cad4def060c7
shihyuan.yu <shihyuay@uci.edu>
2019-05-15 16:52:50 -0700

    fix tuning model

--- pykg2vec/example/tune_model.py
+++ pykg2vec/example/tune_model.py
@@ -1,22 +1,18 @@
-import tensorflow as tf
-from argparse import ArgumentParser
-import importlib
 import sys
 
 sys.path.append("../")
-model_path = "core"
-config_path = "config.config"
 
+from config.hyperparams import KGETuneArgParser
 from utils.bayesian_optimizer import BaysOptimizer
 
-
 def main():
-    parser = ArgumentParser(description='Bayesian HyperParameter Optimizer')
-    parser.add_argument('-m', '--model', default='TransE', type=str, help='Model to tune')
-    args = parser.parse_args()
+   	# getting the customized configurations from the command-line arguments.
+    args = KGETuneArgParser().get_args()
 
+    # initializing bayesian optimizer and prepare data.
     bays_opt = BaysOptimizer(args=args)
 
+    # perform the golden hyperparameter tuning. 
     bays_opt.optimize()
     
 if __name__ == "__main__":
