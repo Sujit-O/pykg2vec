f1b20bfdc23d5198079e98647a520d827fcbebd6
Shih-Yuan Yu <louis29418401@gmail.com>
2019-07-06 18:14:26 -0700

    fix warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -316,3 +316,5 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+numpydoc_show_class_members = False
