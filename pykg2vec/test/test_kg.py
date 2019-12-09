38167adcbaf6e64b2e906a4b38b2dde9a47969e9
baxtree <xi.bai.ed@gmail.com>
2019-12-09 10:26:55 +0000

    add the test on user defined dataset

--- pykg2vec/test/test_kg.py
+++ pykg2vec/test/test_kg.py
@@ -1,3 +1,4 @@
+import os
 from pykg2vec.utils.kgcontroller import KnowledgeGraph
 
 
@@ -60,13 +61,27 @@ def test_fb15k_meta():
     knowledge_graph.dataset.dump()
 
 
-# def test_userdefined_dataset():
+def test_userdefined_dataset():
+    custom_dataset_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resource/custom_dataset")
+    knowledge_graph = KnowledgeGraph(dataset="userdefineddataset", negative_sample="uniform", custom_dataset_path=custom_dataset_path)
+    knowledge_graph.prepare_data()
+    knowledge_graph.dump()
 
-#     import os 
-#     print()
-#     knowledge_graph = KnowledgeGraph(dataset="userdefineddataset", negative_sample="uniform")
-#     knowledge_graph.prepare_data()
-#     knowledge_graph.dump()
+    knowledge_graph.read_cache_data('triplets_train')
+    knowledge_graph.read_cache_data('triplets_test')
+    knowledge_graph.read_cache_data('triplets_valid')
+    knowledge_graph.read_cache_data('hr_t')
+    knowledge_graph.read_cache_data('tr_h')
+    knowledge_graph.read_cache_data('idx2entity')
+    knowledge_graph.read_cache_data('idx2relation')
+    knowledge_graph.read_cache_data('entity2idx')
+    knowledge_graph.read_cache_data('relation2idx')
 
-#     knowledge_graph.dataset.read_metadata()
-#     knowledge_graph.dataset.dump()
+    knowledge_graph.dataset.read_metadata()
+    knowledge_graph.dataset.dump()
+
+    assert knowledge_graph.kg_meta.tot_train_triples == 1
+    assert knowledge_graph.kg_meta.tot_test_triples == 1
+    assert knowledge_graph.kg_meta.tot_valid_triples == 1
+    assert knowledge_graph.kg_meta.tot_entity == 6
+    assert knowledge_graph.kg_meta.tot_relation == 3
