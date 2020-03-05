c9c70cafadc9808ad32a6304fba451f46e7d7a58
louisccc <louis29418401@gmail.com>
2020-03-05 00:43:09 -0800

    modify model test cases.

--- pykg2vec/test/test_model.py
+++ pykg2vec/test/test_model.py
@@ -41,22 +41,13 @@ def testing_function(name, distance_measure=None, bilinear=None, display=False):
     trainer.build_model()
     trainer.train_model()
 
-    
-def test_Complex_args():
+@pytest.mark.parametrize("model_name", ['complex', 'conve', 'convkb', 'distmult', 
+                                        'ntn', 'proje_pointwise', 'rescal', 'rotate', 'slm',
+                                        'transe', 'transh', 'transr', 'transd', 'transm', 'hole',
+                                        'tucker', 'transg'])
+def test_KGE_methods(model_name):
     """Function to test Complex Algorithm with arguments."""
-    testing_function('complex')
-
-def test_ConvE_args():
-    """Function to test ConvE Algorithm with arguments."""
-    testing_function('conve')
-
-def test_ConvKB_args():
-    """Function to test ConvE Algorithm with arguments."""
-    testing_function('convkb')
-    
-def test_DistMult_args():
-    """Function to test DistMult Algorithm with arguments."""
-    testing_function('distmult')
+    testing_function(model_name)
 
 def test_KG2E_EL_args():
     """Function to test KG2E Algorithm with arguments."""
@@ -66,26 +57,6 @@ def test_KG2E_KL_args():
     """Function to test KG2E Algorithm with arguments."""
     testing_function('kg2e', distance_measure="kl_divergence")
 
-def test_NTN_args():
-    """Function to test NTN Algorithm with arguments."""
-    testing_function('ntn')
-
-def test_ProjE_args():
-    """Function to test ProjE Algorithm with arguments."""
-    testing_function('proje_pointwise')
-
-def test_RESCAL_args():
-    """Function to test Rescal Algorithm with arguments."""
-    testing_function('rescal')
-
-def test_RotatE_args():
-    """Function to test RotatE Algorithm with arguments."""
-    testing_function('rotate')
-
-def test_SLM_args():
-    """Function to test SLM Algorithm with arguments."""
-    testing_function('slm')
-
 def test_SMEL_args():
     """Function to test SME Algorithm with arguments."""
     testing_function('sme', bilinear=False)
@@ -94,42 +65,6 @@ def test_SMEB_args():
     """Function to test SME Algorithm with arguments."""
     testing_function('sme', bilinear=True)
 
-def test_transE_args():
-    """Function to test TransE Algorithm with arguments."""
-    testing_function('transe')
-
-def test_transH_args():
-    """Function to test TransH Algorithm with arguments."""
-    testing_function('transh')
-
-def test_transR_args():
-    """Function to test TransR Algorithm with arguments."""
-    testing_function('transr')
-
-def test_TransD_args():
-    """Function to test TransD Algorithm with arguments."""
-    testing_function('transd')
-
-def test_TransM_args():
-    """Function to test TransM Algorithm with arguments."""
-    testing_function('transm')
-
-def test_HoLE_args():
-    """Function to test HolE Algorithm with arguments."""
-    testing_function('hole')
-
-def test_Tucker_args():
-    """Function to test TuckER Algorithm with arguments."""
-    testing_function('tucker')
-
-def test_transG_args():
-    """Function to test TuckER Algorithm with arguments."""
-    testing_function('transg')
-
 def test_transE_display():
     """Function to test transE display."""
-    testing_function('transe', display=True)
-
-def test_transE_args_display():
-    """Function to test TransE Algorithm with arguments for display."""
     testing_function('transe', display=True)
