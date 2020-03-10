90bdfa44f48c1638f1e4041c7615e6b78b323f61
louisccc <louis29418401@gmail.com>
2020-03-09 23:48:54 -0700

    fix distmult

--- pykg2vec/core/DistMult.py
+++ pykg2vec/core/DistMult.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 from pykg2vec.core.KGMeta import ModelMeta, InferenceMeta
 
 
-class DistMult(ModelMeta, InferenceMeta):
+class DistMult(ModelMeta):
     """`EMBEDDING ENTITIES AND RELATIONS FOR LEARNING AND INFERENCE IN KNOWLEDGE BASES`_
 
         DistMult is a simpler model comparing with RESCAL in that it simplifies
@@ -41,49 +41,9 @@ class DistMult(ModelMeta, InferenceMeta):
     """
 
     def __init__(self, config=None):
+        super(DistMult, self).__init__()
         self.config = config
-        self.data_stats = self.config.kg_meta
-        self.tot_ent = self.data_stats.tot_entity
-        self.tot_rel = self.data_stats.tot_relation
         self.model_name = 'DistMult'
-
-    def def_inputs(self):
-        """Defines the inputs to the model.
-
-           Attributes:
-              pos_h (Tensor): Positive Head entities ids.
-              pos_r (Tensor): Positive Relation ids of the triple.
-              pos_t (Tensor): Positive Tail entity ids of the triple.
-              neg_h (Tensor): Negative Head entities ids.
-              neg_r (Tensor): Negative Relation ids of the triple.
-              neg_t (Tensor): Negative Tail entity ids of the triple.
-              test_h_batch (Tensor): Batch of head ids for testing.
-              test_r_batch (Tensor): Batch of relation ids for testing
-              test_t_batch (Tensor): Batch of tail ids for testing.
-        """
-        self.pos_h = tf.placeholder(tf.int32, [None])
-        self.pos_t = tf.placeholder(tf.int32, [None])
-        self.pos_r = tf.placeholder(tf.int32, [None])
-        self.neg_h = tf.placeholder(tf.int32, [None])
-        self.neg_t = tf.placeholder(tf.int32, [None])
-        self.neg_r = tf.placeholder(tf.int32, [None])
-        self.test_h_batch = tf.placeholder(tf.int32, [None])
-        self.test_r_batch = tf.placeholder(tf.int32, [None])
-        self.test_t_batch = tf.placeholder(tf.int32, [None])
-
-    def dissimilarity(self, h, r, t, axis=-1):
-        """Function to calculate dissimilarity measure in embedding space.
-
-        Args:
-            h (Tensor): Head entities ids.
-            r (Tensor): Relation ids of the triple.
-            t (Tensor): Tail entity ids of the triple.
-            axis (int): Determines the axis for reduction
-
-        Returns:
-            Tensors: Returns the dissimilarity measure.
-        """
-        return tf.reduce_sum(h*r*t, axis=axis, keep_dims=False)
 
     def def_parameters(self):
         """Defines the model parameters.
@@ -94,48 +54,15 @@ class DistMult(ModelMeta, InferenceMeta):
                rel_embeddings (Tensor Variable): Lookup variable containing embedding of relations.
                parameter_list  (list): List of Tensor parameters. 
         """
+        num_total_ent = self.config.kg_meta.tot_entity
+        num_total_rel = self.config.kg_meta.tot_relation
         k = self.config.hidden_size
-        with tf.name_scope("embedding"):
-            self.ent_embeddings = tf.get_variable(name="ent_embedding", shape=[self.tot_ent, k],
-                                            initializer=tf.contrib.layers.xavier_initializer(uniform=False))
-            self.rel_embeddings = tf.get_variable(name="rel_embedding", shape=[self.tot_rel, k],
-                                            initializer=tf.contrib.layers.xavier_initializer(uniform=False))
+        
+        emb_initializer = tf.initializers.glorot_normal()
 
+        self.ent_embeddings = tf.Variable(emb_initializer(shape=(num_total_ent, k)), name="ent_embedding")
+        self.rel_embeddings = tf.Variable(emb_initializer(shape=(num_total_rel, k)), name="rel_embedding")
         self.parameter_list = [self.ent_embeddings, self.rel_embeddings]
-
-    def def_loss(self):
-        """Defines the loss function for the algorithm."""
-        pos_h_e, pos_r_e, pos_t_e = self.embed(self.pos_h, self.pos_r, self.pos_t)
-        neg_h_e, neg_r_e, neg_t_e = self.embed(self.neg_h, self.neg_r, self.neg_t)
-
-        score_pos = self.dissimilarity(pos_h_e, pos_r_e, pos_t_e)
-        score_neg = self.dissimilarity(neg_h_e, neg_r_e, neg_t_e)
-
-        regul_term = tf.reduce_mean(pos_r_e**2) + tf.reduce_mean(neg_r_e**2)
-
-        self.loss = tf.reduce_sum(tf.maximum(score_neg - score_pos + 1, 0)) + self.config.lmbda*regul_term
-
-    def test_batch(self):
-        """Function that performs batch testing for the algorithm.
-
-            Returns:
-                Tensors: Returns ranks of head and tail.
-        """
-        h_emb, r_emb, t_emb = self.embed(self.test_h_batch, self.test_r_batch, self.test_t_batch)
-
-        norm_ent_embeddings = tf.nn.l2_normalize(self.ent_embeddings, axis=1)
-
-        score_head = self.dissimilarity(norm_ent_embeddings, 
-                                        tf.expand_dims(r_emb, axis=1), 
-                                        tf.expand_dims(t_emb, axis=1))
-        score_tail = self.dissimilarity(tf.expand_dims(h_emb, axis=1), 
-                                        tf.expand_dims(r_emb, axis=1), 
-                                        norm_ent_embeddings)
-
-        _, head_rank = tf.nn.top_k(tf.negative(score_head), k=self.data_stats.tot_entity)
-        _, tail_rank = tf.nn.top_k(tf.negative(score_tail), k=self.data_stats.tot_entity)
-
-        return head_rank, tail_rank
 
     def embed(self, h, r, t):
         """Function to get the embedding value.
@@ -157,32 +84,67 @@ class DistMult(ModelMeta, InferenceMeta):
 
         return h_emb, r_emb, t_emb
 
-    def get_embed(self, h, r, t, sess=None):
-        """Function to get the embedding value in numpy.
-           
-           Args:
-               h (Tensor): Head entities ids.
-               r (Tensor): Relation ids of the triple.
-               t (Tensor): Tail entity ids of the triple.
-               sess (object): Tensorflow Session object.
+    def dissimilarity(self, h, r, t, axis=-1):
+        """Function to calculate dissimilarity measure in embedding space.
+
+        Args:
+            h (Tensor): Head entities ids.
+            r (Tensor): Relation ids of the triple.
+            t (Tensor): Tail entity ids of the triple.
+            axis (int): Determines the axis for reduction
+
+        Returns:
+            Tensors: Returns the dissimilarity measure.
+        """
+        return tf.reduce_sum(h*r*t, axis=axis, keepdims=False)
+
+    
+    def get_loss(self, pos_h, pos_r, pos_t, neg_h, neg_r, neg_t):
+        """Defines the loss function for the algorithm."""
+        pos_h_e, pos_r_e, pos_t_e = self.embed(pos_h, pos_r, pos_t)
+        neg_h_e, neg_r_e, neg_t_e = self.embed(neg_h, neg_r, neg_t)
+
+        score_pos = self.dissimilarity(pos_h_e, pos_r_e, pos_t_e)
+        score_neg = self.dissimilarity(neg_h_e, neg_r_e, neg_t_e)
+
+        regul_term = tf.reduce_mean(pos_r_e**2) + tf.reduce_mean(neg_r_e**2)
+
+        loss = tf.reduce_sum(tf.maximum(score_neg - score_pos + 1, 0)) + self.config.lmbda*regul_term
+
+        return loss
+
+    def predict(self, h, r, t, topk=-1):
+        """Function that performs prediction for TransE. 
+           shape of h can be either [num_tot_entity] or [1]. 
+           shape of t can be either [num_tot_entity] or [1].
+
+          Returns:
+              Tensors: Returns ranks of head and tail.
+        """
+        h_e, r_e, t_e = self.embed(h, r, t)
+        score = self.dissimilarity(h_e, r_e, t_e)
+        _, rank = tf.nn.top_k(-score, k=topk)
+
+        return rank
+
+    def test_batch(self, h_batch, r_batch, t_batch):
+        """Function that performs batch testing for the algorithm.
 
             Returns:
-                Tensors: Returns head, relation and tail embedding Tensors.
+                Tensors: Returns ranks of head and tail.
         """
-        h, r, t = self.embed(h, r, t)
-        return sess.run([h, r, t])
+        h_emb, r_emb, t_emb = self.embed(h_batch, r_batch, t_batch)
 
-    def get_proj_embed(self, h, r, t, sess):
-        """Function to get the projected embedding value in numpy.
-           
-           Args:
-               h (Tensor): Head entities ids.
-               r (Tensor): Relation ids of the triple.
-               t (Tensor): Tail entity ids of the triple.
-               sess (object): Tensorflow Session object.
+        norm_ent_embeddings = tf.nn.l2_normalize(self.ent_embeddings, axis=1)
 
-            Returns:
-                Tensors: Returns head, relation and tail embedding Tensors.
-        """
-        return self.get_embed(h, r, t, sess)
+        score_head = self.dissimilarity(norm_ent_embeddings, 
+                                        tf.expand_dims(r_emb, axis=1), 
+                                        tf.expand_dims(t_emb, axis=1))
+        score_tail = self.dissimilarity(tf.expand_dims(h_emb, axis=1), 
+                                        tf.expand_dims(r_emb, axis=1), 
+                                        norm_ent_embeddings)
 
+        _, head_rank = tf.nn.top_k(tf.negative(score_head), k=self.config.kg_meta.tot_entity)
+        _, tail_rank = tf.nn.top_k(tf.negative(score_tail), k=self.config.kg_meta.tot_entity)
+
+        return head_rank, tail_rank
