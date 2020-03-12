339c91a3a48a5ba4ae81b51c8a8fbf60ac6aa345
louisccc <louis29418401@gmail.com>
2020-03-11 20:02:53 -0700

    fix conve

--- pykg2vec/core/ConvE.py
+++ pykg2vec/core/ConvE.py
@@ -67,9 +67,16 @@ class ConvE(ModelMeta):
         self.ent_embeddings = tf.Variable(emb_initializer(shape=(num_total_ent, k)), name="ent_embedding")
         self.rel_embeddings = tf.Variable(emb_initializer(shape=(num_total_rel, k)), name="rel_embedding")
         self.b = tf.Variable(emb_initializer(shape=(1, num_total_ent)), name="bias")
-        self.parameter_list = [self.ent_embeddings, self.rel_embeddings, self.b]
+        self.bn0 = tf.keras.layers.BatchNormalization(axis=1)
+        self.inp_drop = tf.keras.layers.Dropout(rate=self.config.input_dropout)
+        self.conv2d_1 = tf.keras.layers.Conv2D(32, [3, 3], strides=(1, 1), padding='valid', use_bias=True, data_format="channels_first")
+        self.bn1 = tf.keras.layers.BatchNormalization(axis=1)
+        self.feat_drop = tf.keras.layers.SpatialDropout2D(self.config.feature_map_dropout, data_format='channels_first')
+        self.fc1 = tf.keras.layers.Dense(units=self.config.hidden_size)
+        self.hidden_drop = tf.keras.layers.Dropout(rate=self.config.hidden_dropout)
+        self.bn2 = tf.keras.layers.BatchNormalization(axis=-1)
 
-        self.def_layer()
+        self.parameter_list = [self.ent_embeddings, self.rel_embeddings, self.b]
 
     def embed(self, h, r, t):
         """Function to get the embedding value.
@@ -88,18 +95,7 @@ class ConvE(ModelMeta):
 
         return emb_h, emb_r, emb_t
 
-    def def_layer(self):
-        """Defines the layers of the algorithm."""
-        self.bn0 = tf.keras.layers.BatchNormalization(axis= -1, trainable=True)
-        self.inp_drop = tf.keras.layers.Dropout(rate=self.config.input_dropout)
-        self.conv2d_1 = tf.keras.layers.Conv2D(32, [3, 3], strides=(1, 1), padding='valid', use_bias=True)
-        self.bn1 = tf.keras.layers.BatchNormalization(axis =-1,trainable=True)
-        self.feat_drop = tf.keras.layers.Dropout(rate=self.config.feature_map_dropout)
-        self.fc1 = tf.keras.layers.Dense(units=self.config.hidden_size)
-        self.hidden_drop = tf.keras.layers.Dropout(rate=self.config.hidden_dropout)
-        self.bn2 = tf.keras.layers.BatchNormalization(axis=-1,trainable=True)
-
-    def forward(self, st_inp):
+    def forward(self, st_inp, batch_size):
         """Implements the forward pass layers of the algorithm."""
         # batch normalization in the first axis
         x = self.bn0(st_inp)
@@ -115,7 +111,7 @@ class ConvE(ModelMeta):
         x = self.feat_drop(x)
         # reshape the tensor to get the batch size
         '''10368 with k=200,5184 with k=100, 2592 with k=50'''
-        x = tf.reshape(x, [-1, self.last_dim])
+        x = tf.reshape(x, [batch_size, -1])
         # pass the feature through fully connected layer, output size = batch size, hidden size
         x = self.fc1(x)
         # dropout in the hidden layer
@@ -125,7 +121,7 @@ class ConvE(ModelMeta):
         # second non-linear activation
         x = tf.nn.relu(x)
         # project and get inner product with the tail triple
-        x = tf.matmul(x, tf.transpose(tf.nn.l2_normalize(self.ent_embeddings, axis=1)))
+        x = tf.matmul(x, self.ent_embeddings, transpose_b=True) # [b, k] * [k, tot_ent]
         # add a bias value
         x = tf.add(x, self.b)
         # sigmoid activation
@@ -134,30 +130,27 @@ class ConvE(ModelMeta):
     def get_loss(self,  h, r, t, hr_t, tr_h):
         """Defines the loss function for the algorithm."""
         h_emb, r_emb, t_emb = self.embed(h, r, t)
-
-        hr_t = tf.cast(tf.sparse.to_dense(tf.sparse.reorder(hr_t)), dtype=tf.float32)
-        tr_h = tf.cast(tf.sparse.to_dense(tf.sparse.reorder(tr_h)), dtype=tf.float32)
-
-        # hr_t = self.hr_t #* (1.0 - self.config.label_smoothing) + 1.0 / self.config.kg_meta.tot_entity
-        # tr_h = self.tr_h #* (1.0 - self.config.label_smoothing) + 1.0 / self.config.kg_meta.tot_entity
-
-        stacked_h = tf.reshape(h_emb, [-1, 10, 20, 1])
-        stacked_r = tf.reshape(r_emb, [-1, 10, 20, 1])
-        stacked_t = tf.reshape(t_emb, [-1, 10, 20, 1])
+        
+        stacked_h = tf.reshape(h_emb, [-1, 1, 10, 20])
+        stacked_r = tf.reshape(r_emb, [-1, 1, 10, 20])
+        stacked_t = tf.reshape(t_emb, [-1, 1, 10, 20])
 
         stacked_hr = tf.concat([stacked_h, stacked_r], 1)
         stacked_tr = tf.concat([stacked_t, stacked_r], 1)
 
         # TODO make two different forward layers for head and tail
-        pred_tails = self.forward(stacked_hr)
-        pred_heads = self.forward(stacked_tr)
+        pred_tails = self.forward(stacked_hr, self.config.batch_size)
+        pred_heads = self.forward(stacked_tr, self.config.batch_size)
 
-        loss_tail_pred = tf.reduce_mean(tf.keras.backend.binary_crossentropy(hr_t, pred_tails))
-        loss_head_pred = tf.reduce_mean(tf.keras.backend.binary_crossentropy(tr_h, pred_heads))
+        hr_t = tf.cast(tf.sparse.to_dense(tf.sparse.reorder(hr_t)), dtype=tf.float32)
+        tr_h = tf.cast(tf.sparse.to_dense(tf.sparse.reorder(tr_h)), dtype=tf.float32)
+        hr_t = hr_t * (1.0 - self.config.label_smoothing) + 1.0 / self.config.kg_meta.tot_entity
+        tr_h = tr_h * (1.0 - self.config.label_smoothing) + 1.0 / self.config.kg_meta.tot_entity
 
-        reg_losses = tf.nn.l2_loss(h_emb) + tf.nn.l2_loss(r_emb) + tf.nn.l2_loss(t_emb)
+        loss_tail_pred = tf.reduce_sum(tf.keras.backend.binary_crossentropy(hr_t, pred_tails))
+        loss_head_pred = tf.reduce_sum(tf.keras.backend.binary_crossentropy(tr_h, pred_heads))
 
-        loss = loss_tail_pred + loss_head_pred + self.config.lmbda * reg_losses
+        loss = loss_tail_pred + loss_head_pred
 
         return loss
 
@@ -165,11 +158,11 @@ class ConvE(ModelMeta):
         h_emb = tf.nn.embedding_lookup(self.ent_embeddings, e)
         r_emb = tf.nn.embedding_lookup(self.rel_embeddings, r)
 
-        stacked_h = tf.reshape(h_emb, [-1, 10, 20, 1])
-        stacked_r = tf.reshape(r_emb, [-1, 10, 20, 1])
+        stacked_h = tf.reshape(h_emb, [-1, 1, 10, 20])
+        stacked_r = tf.reshape(r_emb, [-1, 1, 10, 20])
         
         stacked_hr = tf.concat([stacked_h, stacked_r], 1)
-        pred_tails = -self.forward(stacked_hr)
+        pred_tails = -self.forward(stacked_hr, 1)
 
         _, rank = tf.nn.top_k(pred_tails, k=topk)
 
@@ -179,11 +172,11 @@ class ConvE(ModelMeta):
         t_emb = tf.nn.embedding_lookup(self.ent_embeddings, e)
         r_emb = tf.nn.embedding_lookup(self.rel_embeddings, r)
 
-        stacked_t = tf.reshape(t_emb, [-1, 10, 20, 1])
-        stacked_r = tf.reshape(r_emb, [-1, 10, 20, 1])
+        stacked_t = tf.reshape(t_emb, [-1, 1, 10, 20])
+        stacked_r = tf.reshape(r_emb, [-1, 1, 10, 20])
         
         stacked_tr = tf.concat([stacked_t, stacked_r], 1)
-        pred_heads = -self.forward(stacked_tr)
+        pred_heads = -self.forward(stacked_tr, 1)
 
         _, rank = tf.nn.top_k(pred_heads, k=topk)
 
