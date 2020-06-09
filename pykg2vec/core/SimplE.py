e81c0a2ee5660d2547bb06cab185ba7fb5fa891f
baxtree <xi.bai.ed@gmail.com>
2020-06-09 09:43:09 +0100

    add CP, Complex, DistMult and SimplE models

--- pykg2vec/core/SimplE.py
+++ pykg2vec/core/SimplE.py
@@ -1,10 +1,8 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import math
-import tensorflow as tf
-
+import torch
+import torch.nn as nn
 from pykg2vec.core.KGMeta import ModelMeta
 from pykg2vec.utils.generator import TrainingStrategy
 
@@ -17,28 +15,19 @@ class SimplE(ModelMeta):
         self.model_name = 'SimplE_avg'
         self.training_strategy = TrainingStrategy.POINTWISE_BASED
 
-    def def_parameters(self):
-        """Defines the model parameters.
-
-           Attributes:
-               num_total_ent (int): Total number of entities.
-               num_total_rel (int): Total number of relations.
-               k (Tensor): Size of the latent dimesnion for entities and relations.
-               ent_head_embeddings  (Tensor Variable): Lookup variable containing embedding of the head entities.
-               ent_tail_embeddings  (Tensor Variable): Lookup variable containing embedding of the tail relations.
-               rel_embeddings  (Tensor Variable): Lookup variable containing embedding of the entities.
-               rel_inv_embeddings  (Tensor Variable): Lookup variable containing embedding of the inverse relations.
-               parameter_list  (list): List of Tensor parameters.
-        """
         num_total_ent = self.config.kg_meta.tot_entity
         num_total_rel = self.config.kg_meta.tot_relation
         k = self.config.hidden_size
 
-        emb_initializer = tf.initializers.glorot_normal()
-        self.ent_head_embeddings = tf.Variable(emb_initializer(shape=(num_total_ent, k)), name="ent_head_embedding")
-        self.ent_tail_embeddings = tf.Variable(emb_initializer(shape=(num_total_ent, k)), name="ent_tail_embedding")
-        self.rel_embeddings = tf.Variable(emb_initializer(shape=(num_total_rel, k)), name="rel_embedding")
-        self.rel_inv_embeddings = tf.Variable(emb_initializer(shape=(num_total_rel, k)), name="rel_inv_embedding")
+        self.ent_head_embeddings = nn.Embedding(num_total_ent, k)
+        self.ent_tail_embeddings = nn.Embedding(num_total_ent, k)
+        self.rel_embeddings = nn.Embedding(num_total_rel, k)
+        self.rel_inv_embeddings = nn.Embedding(num_total_rel, k)
+        nn.init.xavier_uniform_(self.ent_head_embeddings.weight)
+        nn.init.xavier_uniform_(self.ent_tail_embeddings.weight)
+        nn.init.xavier_uniform_(self.rel_embeddings.weight)
+        nn.init.xavier_uniform_(self.rel_inv_embeddings.weight)
+
         self.parameter_list = [self.ent_head_embeddings, self.ent_tail_embeddings, self.rel_embeddings, self.rel_inv_embeddings]
 
 
@@ -53,31 +42,34 @@ class SimplE(ModelMeta):
             Returns:
                 Tensors: Returns head, relation and tail embedding Tensors.
         """
-        emb_h1 = tf.nn.embedding_lookup(self.ent_head_embeddings, h)
-        emb_h2 = tf.nn.embedding_lookup(self.ent_head_embeddings, t)
-        emb_r1 = tf.nn.embedding_lookup(self.rel_embeddings, r)
-        emb_r2 = tf.nn.embedding_lookup(self.rel_inv_embeddings, r)
-        emb_t1 = tf.nn.embedding_lookup(self.ent_tail_embeddings, t)
-        emb_t2 = tf.nn.embedding_lookup(self.ent_tail_embeddings, h)
+        emb_h1 = self.ent_head_embeddings(h)
+        emb_h2 = self.ent_head_embeddings(t)
+        emb_r1 = self.rel_embeddings(r)
+        emb_r2 = self.rel_inv_embeddings(r)
+        emb_t1 = self.ent_tail_embeddings(t)
+        emb_t2 = self.ent_tail_embeddings(h)
         return emb_h1, emb_h2, emb_r1, emb_r2, emb_t1, emb_t2
 
     def forward(self, h, r, t):
         h1_e, h2_e, r1_e, r2_e, t1_e, t2_e = self.embed(h, r, t)
 
-        init = tf.reduce_sum(h1_e*r1_e*t1_e, 1) + tf.reduce_sum(h2_e*r2_e*t2_e, 1) / 2.0
-        return -tf.clip_by_value(init, -20, 20)
+        init = torch.sum(h1_e*r1_e*t1_e, 1) + torch.sum(h2_e*r2_e*t2_e, 1) / 2.0
+        return -torch.clamp(init, -20, 20)
 
     def get_reg(self, h, r, t):
         num_batch = math.ceil(self.config.kg_meta.tot_train_triples / self.config.batch_size)
-        regul_term = (tf.nn.l2_loss(self.ent_head_embeddings) + tf.nn.l2_loss(self.ent_tail_embeddings) +
-                      tf.nn.l2_loss(self.rel_embeddings) + tf.nn.l2_loss(self.rel_inv_embeddings)) / num_batch**2
+        regul_term = (self.get_l2_loss(self.ent_head_embeddings) + self.get_l2_loss(self.ent_tail_embeddings) +
+                      self.get_l2_loss(self.rel_embeddings) + self.get_l2_loss(self.rel_inv_embeddings)) / num_batch**2
         return self.config.lmbda * regul_term
+
+    def get_l2_loss(self, embeddings):
+        return torch.sum(embeddings.weight**2) / 2
 
 
 class SimplE_ignr(SimplE):
 
     def __init__(self, config):
-        super(SimplE, self).__init__()
+        super(SimplE_ignr, self).__init__(config)
         self.config = config
         self.model_name = 'SimplE_ignr'
         self.training_strategy = TrainingStrategy.POINTWISE_BASED
@@ -93,18 +85,17 @@ class SimplE_ignr(SimplE):
             Returns:
                 Tensors: Returns head, relation and tail embedding Tensors.
         """
-
-        emb_h = tf.concat([tf.gather(self.ent_head_embeddings, h), tf.gather(self.ent_head_embeddings, t)], 1)
-        emb_r = tf.concat([tf.gather(self.rel_embeddings, r), tf.gather(self.rel_inv_embeddings, r)], 1)
-        emb_t = tf.concat([tf.gather(self.ent_tail_embeddings, t), tf.gather(self.ent_tail_embeddings, h)], 1)
+        emb_h = torch.cat([torch.index_select(self.ent_head_embeddings.weight, 0, h), torch.index_select(self.ent_head_embeddings.weight, 0, t)], 1)
+        emb_r = torch.cat([torch.index_select(self.rel_embeddings.weight, 0, r), torch.index_select(self.rel_inv_embeddings.weight, 0, r)], 1)
+        emb_t = torch.cat([torch.index_select(self.ent_tail_embeddings.weight, 0, t), torch.index_select(self.ent_tail_embeddings.weight, 0, h)], 1)
 
         return emb_h, emb_r, emb_t
 
     def forward(self, h, r, t):
         h_e, r_e, t_e = self.embed(h, r, t)
 
-        init = tf.reduce_sum(h_e*r_e*t_e, 1)
-        return -tf.clip_by_value(init, -20, 20)
+        init = torch.sum(h_e*r_e*t_e, 1)
+        return -torch.clamp(init, -20, 20)
 
     def get_reg(self, h, r, t):
         return 2.0 * super().get_reg(h, r, t)
