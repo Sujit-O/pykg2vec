c38ff29a2e9fd09d93f19b4ad28178e5db228356
baxtree <xi.bai.ed@gmail.com>
2020-06-10 18:26:54 +0100

    fix the ConvE model

--- pykg2vec/core/ConvE.py
+++ pykg2vec/core/ConvE.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import tensorflow as tf
-
+import torch
+import torch.nn as nn
 from pykg2vec.core.KGMeta import ModelMeta
 from pykg2vec.utils.generator import TrainingStrategy
+
 
 class ConvE(ModelMeta):
     """`Convolutional 2D Knowledge Graph Embeddings`_
@@ -43,37 +40,24 @@ class ConvE(ModelMeta):
         self.model_name = 'ConvE'
         self.training_strategy = TrainingStrategy.PROJECTION_BASED
 
-    def def_parameters(self):
-        """Defines the model parameters.
-           
-           Attributes:
-               num_total_ent (int): Total number of entities. 
-               num_total_rel (int): Total number of relations. 
-               k (Tensor): Size of the latent dimesnion for entities and relations.
-               ent_embeddings  (Tensor Variable): Lookup variable containing embedding of the entities.
-               rel_embeddings  (Tensor Variable): Lookup variable containing embedding of the relations.
-               b  (Tensor Variable): Variable storing the bias values.
-               parameter_list  (list): List of Tensor parameters.
-        """        
         num_total_ent = self.config.kg_meta.tot_entity
         num_total_rel = self.config.kg_meta.tot_relation
         k = self.config.hidden_size
 
-        emb_initializer = tf.initializers.glorot_normal()
+        self.ent_embeddings = nn.Embedding(num_total_ent, k)
+        # because conve considers the reciprocal relations,
+        # so every rel should have its mirrored rev_rel in ConvE.
+        self.rel_embeddings = nn.Embedding(num_total_rel*2, k)
+        self.b = nn.Embedding(1, num_total_ent)
 
-        self.ent_embeddings = tf.Variable(emb_initializer(shape=(num_total_ent, k)), name="ent_embedding")
-        self.rel_embeddings = tf.Variable(emb_initializer(shape=(num_total_rel*2, k)), name="rel_embedding") 
-        # because conve considers the reciprocal relations, so every rel should have its mirrored rev_rel in ConvE. 
-        
-        self.b = tf.Variable(emb_initializer(shape=(1, num_total_ent)), name="bias")
-        self.bn0 = tf.keras.layers.BatchNormalization(axis=-1)
-        self.inp_drop = tf.keras.layers.Dropout(rate=self.config.input_dropout)
-        self.conv2d_1 = tf.keras.layers.Conv2D(32, [3, 3], strides=(1, 1), padding='valid', use_bias=True)
-        self.bn1 = tf.keras.layers.BatchNormalization(axis=-1)
-        self.feat_drop = tf.keras.layers.SpatialDropout2D(self.config.feature_map_dropout)
-        self.fc1 = tf.keras.layers.Dense(units=self.config.hidden_size)
-        self.hidden_drop = tf.keras.layers.Dropout(rate=self.config.hidden_dropout)
-        self.bn2 = tf.keras.layers.BatchNormalization(axis=-1)
+        self.bn0 = lambda x: nn.BatchNorm2d(x.shape[1])
+        self.inp_drop = nn.Dropout(self.config.input_dropout)
+        self.conv2d_1 = lambda x: nn.Conv2d(x.shape[1], 32, (3, 3), stride=(1, 1))
+        self.bn1 = lambda x: nn.BatchNorm2d(x.shape[1])
+        self.feat_drop = nn.Dropout2d(self.config.feature_map_dropout)
+        self.fc1 = lambda x: nn.Linear(in_features=x.shape[1], out_features=self.config.hidden_size, bias=True)
+        self.hidden_drop = nn.Dropout(self.config.hidden_dropout)
+        self.bn2 = lambda x: nn.BatchNorm1d(x.shape[1])
         self.parameter_list = [self.ent_embeddings, self.rel_embeddings, self.b]
 
     def embed(self, h, r, t):
@@ -87,48 +71,50 @@ class ConvE(ModelMeta):
             Returns:
                 Tensors: Returns head, relation and tail embedding Tensors.
         """
-        emb_h = tf.nn.embedding_lookup(self.ent_embeddings, h)
-        emb_r = tf.nn.embedding_lookup(self.rel_embeddings, r)
-        emb_t = tf.nn.embedding_lookup(self.ent_embeddings, t)
+        emb_h = self.ent_embeddings(h)
+        emb_r = self.rel_embeddings(r)
+        emb_t = self.ent_embeddings(t)
 
         return emb_h, emb_r, emb_t
 
     def embed2(self, e, r):
-        emb_e = tf.nn.embedding_lookup(self.ent_embeddings, e)
-        emb_r = tf.nn.embedding_lookup(self.rel_embeddings, r)
+        emb_e = self.ent_embeddings(e)
+        emb_r = self.rel_embeddings(r)
         return emb_e, emb_r
 
     def inner_forward(self, st_inp, first_dimension_size):
         """Implements the forward pass layers of the algorithm."""
+        st_inp = st_inp.permute(0, 3, 1, 2)   # convert to channel-first
         # batch normalization in the first axis
-        x = self.bn0(st_inp)
+        x = self.bn0(st_inp)(st_inp)
         # input dropout
         x = self.inp_drop(x)
         # 2d convolution layer, output channel =32, kernel size = 3,3
-        x = self.conv2d_1(x)
+        x = self.conv2d_1(x)(x)
         # batch normalization across feature dimension
-        x = self.bn1(x)
+        x = self.bn1(x)(x)
         # first non-linear activation
-        x = tf.nn.relu(x)
+        x = torch.relu(x)
         # feature dropout
         x = self.feat_drop(x)
         # reshape the tensor to get the batch size
         '''10368 with k=200,5184 with k=100, 2592 with k=50'''
-        x = tf.reshape(x, [first_dimension_size, -1])
+        x = x.view(first_dimension_size, -1)
         # pass the feature through fully connected layer, output size = batch size, hidden size
-        x = self.fc1(x)
+        x = self.fc1(x)(x)
         # dropout in the hidden layer
         x = self.hidden_drop(x)
         # batch normalization across feature dimension
-        x = self.bn2(x)
+        if(x.shape[0] > 1):
+            x = self.bn2(x)(x)
         # second non-linear activation
-        x = tf.nn.relu(x)
+        x = torch.relu(x)
         # project and get inner product with the tail triple
-        x = tf.matmul(x, self.ent_embeddings, transpose_b=True) # [b, k] * [k, tot_ent]
+        x = torch.matmul(x, self.transpose(self.ent_embeddings.weight))    # [b, k] * [k, tot_ent]
         # add a bias value
-        x = tf.add(x, self.b)
+        x = torch.add(x, self.b.weight)
         # sigmoid activation
-        return tf.nn.sigmoid(x)
+        return torch.sigmoid(x)
 
     def forward(self, e, r, direction="tail"):
         if direction == "head":
@@ -136,18 +122,22 @@ class ConvE(ModelMeta):
         else:
             e_emb, r_emb = self.embed2(e, r)
         
-        stacked_e  = tf.reshape(e_emb, [-1, self.config.hidden_size_2, self.config.hidden_size_1, 1])
-        stacked_r  = tf.reshape(r_emb, [-1, self.config.hidden_size_2, self.config.hidden_size_1, 1])
-        stacked_er = tf.concat([stacked_e, stacked_r], 1)
+        stacked_e = e_emb.  view(-1, self.config.hidden_size_2, self.config.hidden_size_1, 1)
+        stacked_r = r_emb.view(-1, self.config.hidden_size_2, self.config.hidden_size_1, 1)
+        stacked_er = torch.cat([stacked_e, stacked_r], 1)
 
-        preds = self.inner_forward(stacked_er, e.get_shape().as_list()[0])
+        preds = self.inner_forward(stacked_er, list(e.shape)[0])
     
         return preds
 
     def predict_tail_rank(self, e, r, topk=-1):
-        _, rank = tf.nn.top_k(-self.forward(e, r, direction="tail"), k=topk)
+        _, rank = torch.topk(-self.forward(e, r, direction="tail"), k=topk)
         return rank
 
     def predict_head_rank(self, e, r, topk=-1):
-        _, rank = tf.nn.top_k(-self.forward(e, r, direction="head"), k=topk)
+        _, rank = torch.topk(-self.forward(e, r, direction="head"), k=topk)
         return rank
+
+    def transpose(self, tensor):
+        dims = tuple(range(len(tensor.shape)-1, -1, -1))    # (rank-1...0)
+        return tensor.permute(dims)
